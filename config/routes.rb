1566adf0f2917194311e001debfb87023624cd1f
tessgold <tgoldmann@smith.edu>
2021-11-09 15:46:05 -0500

    Current Settings page. Accessed by adding /user/settings to URL. To be updated once a logged in page is made.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  
+
   root to: "stations#index"
-  
+
   resources :users, :except => [:index]
   get 'user/delete'
-  
+
   get 'login', to: 'session#login'
   post 'login', to: 'session#create'
-  
-  
-  
+  get 'user/settings'
+
+
   #default route (keep at bottom)
   get ':controller(/:action(/:id))'
-  
+
 end
