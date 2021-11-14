7de27b10d119cd6faef7efcb4dfeac5c0bfb16be
Talia Goldwasser <taliagoldwasser@Talias-MacBook-Pro-4.local>
2021-11-14 18:44:37 -0500

    Added homepage (for real this time?)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
-  root to: "stations#index"
+  root to: "sessions#home"
   
   resources :users, :except => [:index]
   get 'users/delete'
   
   get 'login', to: 'sessions#login'
+  get 'stations', to: 'stations#index'
   
   
   
