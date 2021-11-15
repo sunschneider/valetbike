3f100643b4257b3a15a6ae587c4aa9c7095fe879
tessgold <tgoldmann@smith.edu>
2021-11-15 17:38:23 -0500

    The map shows up now. It doesn't fully work but it is there now.

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,7 +1,4 @@
 Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/YOUR-CLOUDMADE-API-KEY/997/256/{z}/{x}/{y}.png"
 # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
-Leaflet.attribution = "Your attribution statement"
+Leaflet.attribution = "Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>"
 Leaflet.max_zoom = 18
-
-Leaflet.tile_layer = "http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png"
-Leaflet.subdomains = ['otile1', 'otile2', 'otile3', 'otile4']
