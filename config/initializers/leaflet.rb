8c98443b870d59a452180e16ba769f4e7e4eb4cb
tessgold <tgoldmann@smith.edu>
2021-11-15 18:15:55 -0500

    Final Map commit for the first sprint. Map page is correctly links and the map appears on scren. Map does not load fully yet. In addition the user stats page exists. It is not yet linked to anything and does not yet link to user database.

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,4 +1,3 @@
-Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/YOUR-CLOUDMADE-API-KEY/997/256/{z}/{x}/{y}.png"
-# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
-Leaflet.attribution = "Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>"
+Leaflet.tile_layer = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+Leaflet.attribution = "Your attribution statement"
 Leaflet.max_zoom = 18
