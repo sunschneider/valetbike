3a3441944403110c145234ccf8161ab0bf5982ce
Sunshine Schneider <43050520+sunschneider@users.noreply.github.com>
2021-11-15 18:19:54 -0800

    Update user_controller.rb
    trying to resolve merge conflict aaa

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,27 +1,30 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def show
   end
 
   def new
+    @user = User.new
   end
-
+  
   def create
+    @user = User.create(params.require(:user).permit(:first_name))
+    session[:user_id] = @user.id
   end
 
   def edit
   end
-
+  
   def update
   end
 
   def delete
   end
-
+  
   def destroy
   end
-
   def index
     @user = User.all.order(identifier: :asc)
   end
 
+  
 end
