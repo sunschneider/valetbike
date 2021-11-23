e4a857e2daa160d5da0e230348a461c65e368f0c
tessgold <tgoldmann@smith.edu>
2021-11-23 15:51:05 -0500

    fixing file names with plurals

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
 
   root to: "stations#index"
 
-  resources :users, :except => [:index]
-  get 'user/delete'
+  resources :userss, :except => [:index]
+  get 'users/delete'
 
   get 'login', to: 'session#login'
   post 'login', to: 'session#create'
-  get 'user/settings'
+  get 'users/settings'
   get 'stations/map'
-  get 'user/stats'
+  get 'users/stats'
 
 
   #default route (keep at bottom)
