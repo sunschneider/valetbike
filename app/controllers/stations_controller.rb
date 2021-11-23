7e0bf68909bddceba32f0f9173d61efd2d0e94fc
tessgold <tgoldmann@smith.edu>
2021-11-23 15:42:05 -0500

    added latitude and longitude float variables to the stations model. new stations now require lat and long values.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,4 +4,7 @@ class StationsController < ApplicationController
     @stations = Station.all.order(identifier: :asc)
   end
 
+  def map
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_194800) do
+ActiveRecord::Schema.define(version: 2021_11_23_180117) do
 
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2021_11_04_194800) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "longitude"
+    t.float "latitude"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
