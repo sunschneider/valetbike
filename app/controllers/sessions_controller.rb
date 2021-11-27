f2f14d577737047b07e36cf13086d9bead6a0140
Domily11 <ckung@smith.edu>
2021-11-27 15:03:02 -0500

    Added in Login and logout functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,28 @@
 class SessionsController < ApplicationController
-  def login
+  def home
+    if logged_in? 
+  # Links for logged-in users
+      redirect_to(stations_path)
+    end
   end
+   
+   def login
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+       log_in(@user)
+       redirect_to (stations_path)
+    else
+       flash.now[:danger] = 'Invalid email/password combination'
+       render('login')
+    end
+ end
+
+ def destroy
+   log_out()
+   redirect_to(root_path)
+ end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   get 'users/delete'
   
   get 'login', to: 'sessions#login'
+  delete 'logout', to: 'sessions#destroy'
   get 'stations', to: 'stations#index'
   
   
