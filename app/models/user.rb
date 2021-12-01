ba360065f613f2f92acd6ffd597ec4f32567c878
Talia Goldwasser <talia.goldwasser@gmail.com>
2021-11-30 20:25:47 -0500

    bikes can belong to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :email,
                         :password
+  
+  has_many :rented_bikes, class_name: :Bike, foreign_key: :current_user_id
 end
