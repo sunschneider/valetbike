7f2548f77cf2d25fd3b7c47975d1c6183b355fd4
tessgold <tgoldmann@smith.edu>
2021-12-06 16:53:04 -0500

    everything is done! KEEP MY VERSION OF THE USERS/NEW I fixed a bug and this version works

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -6,10 +6,16 @@ class StationsController < ApplicationController
 
   def show
     @stations = Station.where.not(latitude: 'Empty', longitude: "Empty").pluck(:latitude, :longitude, :name)
+    @bikeCount = []
     @markers = []
+    @count = 0
+    for station in Station.all do
+      @bikeCount << station.docked_bikes.count
+    end
     @stations.each do |c|
-      @hash = {:latlng => c, :popup => c[-1]}
+      @hash = {:latlng => c, :popup => c[-1] + ", " + @bikeCount[@count].to_s + " Bikes"}
       @markers << @hash
+      @count += 1
     end
   end
 
